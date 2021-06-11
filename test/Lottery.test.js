eb67c711b088f4bfa285f8fe8d9011b258a6cd6f
Vlad <ilin96inbox@gmail.com>
2021-06-11 22:02:24 +0900

    test manager

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -63,4 +63,13 @@ describe("Lottery Contract", () => {
       assert(error);
     }
   });
+  
+  it('only manager can call pickWinner',async () => {
+    try {
+      await lottery.methods.pickWinner().send({ from: accounts[1]})
+      assert(false);
+    } catch (error) {
+      assert(error);
+    }
+  });
 });
